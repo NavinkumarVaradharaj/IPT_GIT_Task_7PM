948e2cfbd1c0ebf11237066d03aeaccef9558a86
Muralir27 <muralir270197@gmail.com>
2023-08-17 20:10:49 +0530

    LoginPage

--- src/main/java/com/NykaaIPT_Aug_TestNG/pom/LoginPage.java
+++ src/main/java/com/NykaaIPT_Aug_TestNG/pom/LoginPage.java
@@ -1,5 +1,31 @@
 package com.NykaaIPT_Aug_TestNG.pom;
 
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.NykaaIPT_Aug_TestNG.base.BaseClass;
+
+public class LoginPage extends BaseClass implements Login {
+	
+	public LoginPage(){
+		PageFactory.initElements(driver, this); 
+	}
+	
+	@FindBy(xpath = signIn_btn_xpath)
+	private WebElement signIn_btn;
+	
+	@FindBy(xpath = sign_with_mobile_xpath)
+	private WebElement sign_with_mobile;
+
+	public WebElement getSignIn_btn() {
+		return signIn_btn;
+	}
+
+	public WebElement getSign_with_mobile() {
+		return sign_with_mobile;
+	}
+	
+	
 
 }

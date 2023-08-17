21af9d427c797bc7ecfa8e7788e7c624d55ccdd5
Vijayarajan-v <vijayvr8@gmail.com>
2023-08-17 20:16:23 +0530

    CartPage_bag Icon

--- src/main/java/com/NykaaIPT_Aug_TestNG/pom/CartPage.java
+++ src/main/java/com/NykaaIPT_Aug_TestNG/pom/CartPage.java
@@ -1,5 +1,31 @@
 package com.NykaaIPT_Aug_TestNG.pom;
 
-public class CartPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.NykaaIPT_Aug_TestNG.base.BaseClass;
+
+public class CartPage extends BaseClass implements Cart {
+
+	public CartPage () {
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	@FindBy(xpath=bag_icon_xpath)
+	private WebElement bag_icon;
+	
+	@FindBy(xpath=bag_frame_xpath)
+	private WebElement bag_frame;
+
+	public WebElement getBag_icon() {
+		return bag_icon;
+	}
+
+	public WebElement getBag_frame() {
+		return bag_frame;
+	}
+	
+	
 }
